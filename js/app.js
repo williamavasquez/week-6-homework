9c3b7c326aa1f8aef2e2337bf41080f72d373197
williamavasquez <william.a.vasquez@gmail.com>
2016-03-13 23:26:59 -0400

    working script, missing css

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
     var movies = ["batman"];
 
+    // Create a button of each item inputed
     function renderButtons(){ 
         $('#buttonsView').empty();
 
@@ -14,51 +15,62 @@ $(document).ready(function(){
         }
     }
 
+    // show the gif when they press the button
     function displayMovieInfo(){
-        // empty the movieView div to add new ones
-         $('#moviesView').empty();
-
         var movie = $(this).attr('data-name');
         var queryURL = "http://api.giphy.com/v1/gifs/search?q="+movie+"&api_key=dc6zaTOxFJmzC&limit=10";
-        var DisplayedGif=[];
 
         $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-
             gifArray = (response.data);
-         
+
          for (var i = 0; i < gifArray.length; i++) {
+            var BoxDiv = $("<div>").addClass("BoxDiv");
+
+            // Get the rating and create a div for the text
+            var p = $("<p>").addClass("rating").text("Rating: "+gifArray[i].rating); 
+
+            // Get the gif and create a div for the still image 
             var showGif = $('<img>')
             showGif.attr({"data-trivia": i, "data-status": 0});
-            // showGif.attr("data-trivia",i);
             showGif.attr("src",gifArray[i].images.original_still.url).addClass("gifImage");
-            DisplayedGif.push(response.data);
-            $("#moviesView").append(showGif);
+
+            // append the image and rating to the main div=BoxDiv
+            BoxDiv.append(p);
+            BoxDiv.append(showGif);
+
+            // add the div to the page
+            $("#moviesView").append(BoxDiv);            
          };
     });
     }
 
-    $('#addMovie').on('click', function(){
-
-        var movie = $('#movie-input').val().trim();
-        movies.push(movie);
-        renderButtons();
-        return false;
+    // get the information from the form push it to the renderButtons
+        $('#addMovie').on('click', function(){
+            var movie = $('#movie-input').val().trim();
+            movies.push(movie);
+            renderButtons();
+            return false;
     });
     
+    // play and pause the gif
     function playGif(){
+        // play gif
         if (($(this).attr('data-status'))==0) {
+            // change the status to play the gif
             $(this).attr("data-status",1);
-
+            // swap still image for gif
              StartGif = ($(this).data('trivia'));
             $(this).attr("src",gifArray[StartGif].images.downsized.url);
         } else {
-            $(this).attr("data-status",1);
-            
+            // change the status to still image
+            $(this).attr("data-status",0);
+            // swap still image for gif
             StartGif = ($(this).data('trivia'));
            $(this).attr("src",gifArray[StartGif].images.original_still.url);
         }
     }
 
+    // pass the on click function to all new objects
     $(document).on('click', '.movie', displayMovieInfo);
     $(document).on('click','.gifImage',playGif);
 })
